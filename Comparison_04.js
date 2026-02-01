d1ebad0d5900597f7824dad3afa61e105499e452
MdRaj87 <mdraj87970@gmail.com>
2026-02-01 14:15:39 +0530

    add

---
+++ Comparison_04.js
@@ -0,0 +1,18 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log( undefined < 0);
+
+console.log("2" === 2);
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// console.log(id);
+// console.log(anotherId);
