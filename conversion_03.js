94f85e9915e88a6045b2988df5173dc91113e0e4
MdRaj87 <mdraj87970@gmail.com>
2026-02-01 13:13:47 +0530

    second commit

--- conversion_03.js
+++ conversion_03.js
@@ -1,8 +1,45 @@
 let isLoggedIn = "Baalak"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 ==> false
-// "" => false
-// "Baalak" => true
+// // // 1 => true; 0 ==> false
+// // // "" => false
+// // // "Baalak" => true
+
+let score = 25;
+let name = "Baalak";
+let isBoy = true;
+let result;
+let newScore = null;        // => typeof object
+let bigNumber = 12587n; 
+
+// // console.table([typeof (score), typeof name, typeof isBoy, typeof result, typeof newScore, typeof bigNumber]);
+
+
+// // // conversion 👇👇👇👇👇👇
+ 
+// let age = "20";
+
+// //console.log(typeof(age));
+
+let valueinNumber = Number (age);
+
+// // console.log(typeof valueinNumber);
+
+let naam = "Baaalak";
+
+let naamInNumber = Number (naam);
+// console.log(typeof naamInNumber);
+// console.log(naamInNumber);
+
+
+let age = "5";
+
+
+// Number("5") + 5 = 10
+
+// let newage = Number(age);
+console.log(Number(age) + 5);
+
+
