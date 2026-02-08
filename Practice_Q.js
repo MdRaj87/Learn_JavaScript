6a055b4b200fe55012efcb6b9b1257734e7dfcbc
MdRaj87 <mdraj87970@gmail.com>
2026-02-08 20:40:10 +0530

    new

---
+++ Practice_Q.js
@@ -0,0 +1,10 @@
+let name = "Baalak";
+
+let newName = new String(Light);
+
+console.log(newName);
+
+console.log([name.length]);
+console.log(typeof name);
+console.log(name[0]); // print index  
+console.log (name[name.length-1]);
---
+++ string_06.js
@@ -0,0 +1,30 @@
+const name = "Baalak";
+const friends = 2;
+
+
+// console.log(name + friends + " my total frineds ") (old format to write code)
+
+//console.log(`My name is ${name} and my total friends is ${friends}`) // (new/modern format to write code)
+
+//console.log(name[0]);  // index print kar sakte hai
+
+
+//console.log(name[5]); 
+//console.log([name.length]); // total length
+
+// Concatenation (adding string together)
+
+let firstname = "Baalak";
+let lastname = "Raj";
+
+let fullname = firstname + " " + lastname ;
+//console.log(fullname);
+//console.log(`${firstname} ${lastname}`); // Medern style
+
+const gameName = new String('Light');
+//console.log(gameName);
+
+//console.log(firstname.indexOf('a')); // a letter kitna index me hai
+
+console.log(firstname.indexOf('a', 2)); // Means "a" search karna 2 index se start karo
+
